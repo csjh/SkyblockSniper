7508a466eb3d1ff942e4c0fa3bcb8c0814843f57
csjh <61282104+csjh@users.noreply.github.com>
2021-09-08 21:56:48 -0400

    added some comments

--- SkyblockSniper.py
+++ SkyblockSniper.py
@@ -8,9 +8,6 @@ from timeit import default_timer
 import pandas as pd
 import requests
 
-frequency = 500
-duration = 500
-
 c = requests.get("https://api.hypixel.net/skyblock/auctions?page=0")
 resp = c.json()
 now = resp['lastUpdated']
@@ -19,6 +16,7 @@ toppage = resp['totalPages']
 results = []
 prices = {}
 
+# stuff to remove
 REFORGES = [" ✦", "⚚ ", " ✪", "✪", "Stiff ", "Lucky ", "Jerry's ", "Dirty ", "Fabled ", "Suspicious ", "Gilded ", "Warped ", "Withered ", "Bulky ", "Stellar ", "Heated ", "Ambered ", "Fruitful ", "Magnetic ", "Fleet ", "Mithraic ", "Auspicious ", "Refined ", "Headstrong ", "Precise ", "Spiritual ", "Moil ", "Blessed ", "Toil ", "Bountiful ", "Candied ", "Submerged ", "Reinforced ", "Cubic ", "Warped ", "Undead ", "Ridiculous ", "Necrotic ", "Spiked ", "Jaded ", "Loving ", "Perfect ", "Renowned ", "Giant ", "Empowered ", "Ancient ", "Sweet ", "Silky ", "Bloody ", "Shaded ", "Gentle ", "Odd ", "Fast ", "Fair ", "Epic ", "Sharp ", "Heroic ", "Spicy ", "Legendary ", "Deadly ", "Fine ", "Grand ", "Hasty ", "Neat ", "Rapid ", "Unreal ", "Awkward ", "Rich ", "Clean ", "Fierce ", "Heavy ", "Light ", "Mythic ", "Pure ", "Smart ", "Titanic ", "Wise ", "Bizarre ", "Itchy ", "Ominous ", "Pleasant ", "Pretty ", "Shiny ", "Simple ", "Strange ", "Vivid ", "Godly ", "Demonic ", "Forceful ", "Hurtful ", "Keen ", "Strong ", "Superior ", "Unpleasant ", "Zealous "]
 
 START_TIME = default_timer()
@@ -27,32 +25,38 @@ def fetch(session, page):
     global toppage
     base_url = "https://api.hypixel.net/skyblock/auctions?page="
     with session.get(base_url + page) as response:
+        # puts response in a dict
         data = response.json()
         toppage = data['totalPages']
         if data['success']:
             toppage = data['totalPages']
             for auction in data['auctions']:
-                if not auction['claimed'] and 'bin' in auction:
+                if not auction['claimed'] and 'bin' in auction: # if the auction isn't a) claimed and is b) BIN
+                    # removes level if it's a pet
                     index = re.sub("\[[^\]]*\]", "", auction['item_name']) + auction['tier']
+                    # removes reforges and other yucky characters
                     for reforge in REFORGES: index = index.replace(reforge, "")
+                    # if the current item already has a price in the prices map, the price is updated
                     if index in prices:
                         if prices[index][0] > auction['starting_bid']:
                             prices[index][1] = prices[index][0]
                             prices[index][0] = auction['starting_bid']
                         elif prices[index][1] > auction['starting_bid']:
                             prices[index][1] = auction['starting_bid']
+                    # otherwise, it's added to the prices map
                     else:
-                        prices[index] = [float("inf"), float("inf")]
-
+                        prices[index] = [auction['starting_bid'], float("inf")]
+                        
+                    # if the auction fits in some parameters
                     if prices[index][1] > 1000000 and prices[index][0] < prices[index][1]/2 and auction['start']+60000 > now:
                         results.append([auction['uuid'], auction['item_name'], auction['starting_bid'], index])
         return data
 
 async def get_data_asynchronous():
+    # puts all the page strings
     pages = [str(x) for x in range(toppage)]
     with ThreadPoolExecutor(max_workers=10) as executor:
         with requests.Session() as session:
-            # Set any session parameters here before calling `fetch`
             loop = asyncio.get_event_loop()
             START_TIME = default_timer()
             tasks = [
@@ -61,39 +65,51 @@ async def get_data_asynchronous():
                     fetch,
                     *(session, page) # Allows us to pass in multiple arguments to `fetch`
                 )
+                # runs for every page
                 for page in pages if int(page) < toppage
             ]
             for response in await asyncio.gather(*tasks):
                 pass
 
 def main():
+    # Resets variables
     global results, prices, START_TIME
     START_TIME = default_timer()
     results = []
     prices = {}
+    
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     future = asyncio.ensure_future(get_data_asynchronous())
     loop.run_until_complete(future)
-    done = default_timer() - START_TIME
+    
+    # Makes sure all the results are still up to date
     results = [[entry, prices[entry[3]][1]] for entry in results if (entry[2] > 1000000 and prices[entry[3]][1] != float('inf') and prices[entry[3]][0] == entry[2] and prices[entry[3]][0] < prices[entry[3]][1]/2)]
-    if len(results):
+    
+    if len(results): # if there's results to print
         df=pd.DataFrame(['/viewauction ' + str(max(results, key=lambda entry:entry[0][2])[0][0])])
-        df.to_clipboard(index=False,header=False)
-        winsound.Beep(frequency, duration)
-        print(results, done, len(results))
+        df.to_clipboard(index=False,header=False) # copies most valuable auction to clipboard (usually just the only auction cuz very uncommon for there to be multiple
+        
+        winsound.Beep(500, 500) # emits a frequency 500hz, for 500ms
+        
+        done = default_timer() - START_TIME
+        print(results, done, len(results)) # prints auction to console
 
 main()
 
-def test():
+def dostuff():
     global now, toppage
-    threading.Timer(1, test).start()
+    threading.Timer(1, dostuff).start()
+    
+    # gets first page to initialize some variables
     c = requests.get("https://api.hypixel.net/skyblock/auctions?page=0")
     resp = c.json()
     tempnow = resp['lastUpdated']
+    
+    # if the new time is larger than the old time, main is executed
     if tempnow > now:
         now = tempnow
         toppage = resp['totalPages']
         main()
 
-test()
+dostuff()
