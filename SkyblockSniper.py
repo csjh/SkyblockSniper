ba76dd5f32a2da1bfd433c34e44e1330036c27ba
csjh <61282104+csjh@users.noreply.github.com>
2021-09-08 23:01:53 -0400

    said bazaar instead of ah whoops

--- SkyblockSniper.py
+++ SkyblockSniper.py
@@ -97,7 +97,7 @@ def main():
         
         done = default_timer() - START_TIME
         for result in results:
-            print("Auction UUID: " + str(result[0][0]) + " | Item Name: " + str(result[0][1]) + " | Item price: {:,}".format(result[0][2]), " | Second lowest BIN: {:,}".format(result[1]) + " | Time to refresh Bazaar: " + str(done))
+            print("Auction UUID: " + str(result[0][0]) + " | Item Name: " + str(result[0][1]) + " | Item price: {:,}".format(result[0][2]), " | Second lowest BIN: {:,}".format(result[1]) + " | Time to refresh AH: " + str(done))
         print()
 
 main()
